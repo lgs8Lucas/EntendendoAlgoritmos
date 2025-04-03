5e8420ef711e60ade6b571e7ae409e66fc9a31e0
Lucas <lgs.08lucas@gmail.com>
2025-04-03 15:13:51 -0300

    Criado um grafo.

---
+++ capitulo-5/grafo.js
@@ -0,0 +1,8 @@
+const grafo = {};
+grafo["Lucas"] = ["Ana", "Pedro", "Marcos"];
+grafo["Ana"] = ["Lucas", "João", "Leticia"];
+grafo["Pedro"] = ["Lucas", "Maria"];
+grafo["Marcos"] = [];
+grafo["João"] = ["Ana"];
+grafo["Leticia"] = ["Ana"];
+grafo["Maria"] = [];
